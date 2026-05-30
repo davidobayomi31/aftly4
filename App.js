7e2f50d43de347de058a84c4ca63d0ac5921fbc2
David Obayomi <Davidobayomi3@gmail.com>
2026-05-30 12:51:16 -0400

    update base screen

--- App.js
+++ App.js
@@ -1,38 +1,66 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { view, Text,StyleSheet } from 'react-native';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+// homescreen
+function HomeScreen(){
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="dark" />
-      <View style={styles.header}>
-        <Text style={styles.logo}>Aftly</Text>
-        <Text style={styles.marina}>Blue Beacon Marina</Text>
-      </View>
-    </SafeAreaView>
+    <View style = {style.screen}> 
+      <Text style = {styles.title}>Marina Card</Text>
+    </View>
+  );
+}
+//feed
+function FeedScreen(){
+  return (
+    <view style = {style.screen}>
+      <Text style={styles.title}>Marina Feed</Text>
+    </view>
+  );
+} 
+//where users log there trip
+function LogScreen () {
+  return (
+    <view style={styles.screen}>
+      <Text style={styles.title}>Log a Trip </Text>
+    </view>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+export default function App(){
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActivateTintColor: '#0EA5E9',
+          tabBarInactiveTintColor: '#9CA3AF',
+          tabBarStyle:{ borderTopColor: '#F0F0F0'},
+          headerShown: false,
+        }}> 
+        <Tab.Screen name = "Home" component ={HomeScreen} />
+        <Tab.Screen name = "Feed" componet = {FeedScreen} />
+        <Tab.Screen name = "log" component = {LogScreen} />
+
+        </Tab.Navigator>
+
+    </NavigationContainer>
+  );
+}
+
+const style = StyleSheet.create({
+  screen: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
+    backgroundColor:'#FFFFFF',
+    alignItems:'center',
+    justifyContent: 'center',
   },
-  header: {
-    paddingHorizontal: 20,
-    paddingTop: 20,
-    paddingBottom: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#F0F0F0',
+  title: {
+    fontsize: 24, 
+    fontweight: bold,
+    color: '111827',
   },
-  logo: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#0EA5E9',
-  },
-  marina: {
-    fontSize: 13,
-    color: '#9CA3AF',
-    marginTop: 2,
-  },
-});
+})
+
+
+
